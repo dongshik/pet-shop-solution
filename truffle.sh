064e19c17975ee55d744ceeefc414ce79c6177ae
dongsik <dongshik.lee@gmail.com>
2018-09-07 15:26:05 +0900

    add solution

---
+++ truffle.sh
@@ -0,0 +1,10 @@
+rm -rf build
+
+truffle compile
+
+truffle migrate
+
+# https://chrome.google.com/webstore/search/metamask
+# mnemonic for ubuntu
+# alert fuel tool only bleak angry language erode fix pond amused stuff
+# http://127.0.0.1:7545
